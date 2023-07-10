1617c0ec4f6718f42408be0209ac737f11769815
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-07-10 15:51:26 -0400

    Start working on js: added querySelectors, added forEach addEventListener for numberBtns to display on calculator...need to figure out how to keep values in display until operBtn is pressed

--- calculator.js
+++ calculator.js
@@ -1,27 +1,63 @@
-const add = function(a, b) {
-    return a + b;
-  };
+const container = document.querySelector('.container');
+const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.numBtns');
+
+// Operator buttons
+const equal = document.querySelector('#equalBtn');
+const clear = document.querySelector('#clearBtn');
+const addBtn = document.querySelector('#addBtn');
+const subtractBtn = document.querySelector('#subBtn');
+const multiplyBtn = document.querySelector('#multBtn');
+const divideBtn = document.querySelector('#divideBtn');
+const negPosBtn = document.querySelector('#negPosBtn');
+const percentBtn = document.querySelector('#percentBtn');
+
+// When each number button is pressed
+// Display in display input
+numbers.forEach(number => number.addEventListener('click', (e) => {
+  display.value = e.target.textContent;
+}))
+// Keep selected numBtn in display until operator button is pushed
+// Or keep the value in display until operBtn pressed
+// If numbers are clicked
+// Keep display.value in display box
+
+// Operator button functions
+// Link this to add button
+// const add = function(a, b) {
+//     return a + b;
+//   };
   
-  const subtract = function(a, b) {
-      let difference = a - b;
-    return difference;
-  };
+//   // Link this to subtract button
+//   const subtract = function(a, b) {
+//       let difference = a - b;
+//     return difference;
+//   };
 
-  const multiply = function(array) {
-    const initialValue = 1;
-    const sumMultiplied = array.reduce((previousValue, currentValue) => previousValue * currentValue, initialValue);
-    return sumMultiplied;
-  };
+//   // Link this to multiplication button
+//   const multiply = function(array) {
+//     const initialValue = 1;
+//     const sumMultiplied = array.reduce((previousValue, currentValue) => previousValue * currentValue, initialValue);
+//     return sumMultiplied;
+//   };
 
-  const divide = function(array) {
-    const initialValue = 1;
-    const sumDivided = array.reduce((previousValue, currentValue) => previousValue / currentValue, initialValue);
-    return sumDivided;
-  }
+//   // Link this to division button
+//   const divide = function(array) {
+//     const initialValue = 1;
+//     const sumDivided = array.reduce((previousValue, currentValue) => previousValue / currentValue, initialValue);
+//     return sumDivided;
+//   }
 
-  module.exports = {
-    add,
-    subtract,
-    multiply,
-    divide
-  }
+  // Equal button should put results in display
+// equal function(){};
+
+
+
+
+
+  // module.exports = {
+  //   add,
+  //   subtract,
+  //   multiply,
+  //   divide
+  // }
