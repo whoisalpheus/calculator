ab82e5659848cf2cdb609adedd37ce592269b158
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-07-17 16:34:06 -0400

    Clean up and make more simple, trying to link the button clicks to the operation functions

--- calculator.js
+++ calculator.js
@@ -1,91 +1,118 @@
 const container = document.querySelector('.container');
-const display = document.querySelector('#display');
+const display = document.querySelector('#displayBox');
 const numbers = document.querySelectorAll('.numBtns');
+const operators = document.querySelectorAll('.operBtns');
 
 // Operator buttons
 const equal = document.querySelector('#equalBtn');
 const clear = document.querySelector('#clearBtn');
-const addBtn = document.querySelector('#addBtn');
-const subtractBtn = document.querySelector('#subBtn');
-const multiplyBtn = document.querySelector('#multBtn');
-const divideBtn = document.querySelector('#divideBtn');
+// const addBtn = document.querySelector('#addBtn');
+// const subtractBtn = document.querySelector('#subBtn');
+// const multiplyBtn = document.querySelector('#multBtn');
+// const divideBtn = document.querySelector('#divideBtn');
 const negPosBtn = document.querySelector('#negPosBtn');
 const percentBtn = document.querySelector('#percentBtn');
+
 let num1 = '';
 let num2 = '';
 let operator = '';
 
-// function operate(num1, operator, num2) {
-//   const num1 = display.value;
-// }
-
 // When each number button is pressed
 // Display in display input
 
-numbers.forEach(number => number.addEventListener('click', (e) => {
-  display.value += e.target.textContent;
+const numClick = numbers.forEach(number => number.addEventListener('click', (e) => {
+  display.textContent += e.target.textContent;
 }))
-
-// Keep selected numBtn in display until operator button is pushed
-// Or keep the value in display until operBtn pressed
-// If numbers are clicked
-// Keep display.value in display box
 // +=
+
+const opClick = operators.forEach(operator => operator.addEventListener('click', (e) => {
+  display.textContent += e.target.textContent;
+}))
 
 // Clear button
 clear.addEventListener('click', clearDisplay);
 
 function clearDisplay() {
-  display.value = '';
+  display.textContent = '';
 }
 
-// Highlight operation function buttons (+, -, *, /)
-addBtn.addEventListener('click', () => {
-  addBtn.style.border = 'thick double #07562f';
-})
-subtractBtn.addEventListener('click', () => {
-  subtractBtn.style.border = 'thick double #07562f';
-})
-
-divideBtn.addEventListener('click', () => {
-  divideBtn.style.border = 'thick double #07562f';
-})
-
-multiplyBtn.addEventListener('click', () => {
-  multiplyBtn.style.border = 'thick double #07562f';
-})
-
-// // Operator button functions
-// // Link this to add button
-// const add = function(num1, num2) {
-//     return num1 + num2;
-//   };
+// Operator button functions
+// Link this to add button
+function add(num1, num2) {
+    return num1 + num2;
+  };
   
-//   // Link this to subtract button
-//   const subtract = function(num1, num2) {
-//       let difference = num1 - num2;
-//     return difference;
-//   };
+  // Link this to subtract button
+  function subtract(num1, num2) {
+    return num1 - num2;
+  };
 
-//   // Link this to multiplication button
-//   const multiply = function(array) {
-//     const initialValue = 1;
-//     const sumMultiplied = array.reduce((previousValue, currentValue) => previousValue * currentValue, initialValue);
-//     return sumMultiplied;
-//   };
+  // Link this to multiplication button
+  function multiply(num1, num2) {
+    return num1 * num2;
+  };
 
-//   // Link this to division button
-//   const divide = function(array) {
-//     const initialValue = 1;
-//     const sumDivided = array.reduce((previousValue, currentValue) => previousValue / currentValue, initialValue);
-//     return sumDivided;
-//   }
+  // Link this to division button
+  function divide(num1, num2) {
+    return num1 / num2;
+  }
+
+
+
+// // If addBtn is clicked:
+// addBtn.addEventListener('click', add);
+// // If subBtn is clicked:
+// subBtn.addEventListener('click', subtract);
+// // If multiplyBtn is clicked:
+// multiplyBtn.addEventListener('click', multiply);
+// // If divideBtn is clicked:
+// divideBtn.addEventListener('click', divide);
+
+
+// Have to link the button clicks with the operate function somehow
+
 
   // Equal button should put results in display
 // equal function(){};
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Highlight operation function buttons (+, -, *, /)
+// addBtn.addEventListener('click', () => {
+//   addBtn.style.border = 'thick double #07562f';
+// })
+// subtractBtn.addEventListener('click', () => {
+//   subtractBtn.style.border = 'thick double #07562f';
+// })
+
+// divideBtn.addEventListener('click', () => {
+//   divideBtn.style.border = 'thick double #07562f';
+// })
+
+// multiplyBtn.addEventListener('click', () => {
+//   multiplyBtn.style.border = 'thick double #07562f';
+// })
 
 
   // module.exports = {
